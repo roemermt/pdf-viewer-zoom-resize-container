2b49ede2e8f663c70e418ea05325bf0354f3fa20
Michael Roemer <66832348+roemermt@users.noreply.github.com>
2024-10-08 09:47:10 -0400

    add redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExamplePdfViewerComponent } from './example-pdf-viewer/example-pdf-viewer.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'example-pdf-viewer', pathMatch: 'full' },  
   { path: 'example-pdf-viewer', component: ExamplePdfViewerComponent },
 ];
 
